83e2bc7c3b4f84c934f1c042465a905be801b65e
Shivanand <datasnekatpure@gmail.com>
2025-06-09 14:23:04 +0530

    updated app.py

--- langserve_backend/main.py
+++ langserve_backend/main.py
@@ -5,6 +5,4 @@ from disgonostics_graph import build_graph
 app = FastAPI()
 graph = build_graph()
 
-# Remove all authorization logic
-# @app.post("/diagnose")
 add_routes(app, graph, path="/diagnose")
